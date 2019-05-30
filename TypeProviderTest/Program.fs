cb5c93d60943aef8db04fd717c0ef5ff3a307bd0
Alexandre Konioukhov <alexan1@gmail.com>
2019-05-30 10:14:01 -0400

    Add project files.

---
+++ TypeProviderTest/Program.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open FSharp.Data
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    let data = WorldBankData.GetDataContext()
+
+    let result =
+        data
+            .Countries.``United Kingdom``
+            .Indicators.``Gross capital formation (% of GDP)``
+            |> Seq.maxBy fst
+
+    let dada1 = 
+    //printfn "%i" result
+    0 // return an integer exit code
